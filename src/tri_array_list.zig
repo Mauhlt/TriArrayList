7ac4d8b5953321bb317bfb28c4adf4e3b1eba458
Brian <bphilip777@gmail.com>
2026-02-03 19:10:00 -0500

    Added more useful fns

--- src/tri_array_list.zig
+++ src/tri_array_list.zig
@@ -55,15 +55,19 @@ pub fn Aligned(comptime T: type, comptime alignment: ?Alignment) type {
         /// Buffer deterimines capacity + length set to 0
         /// All fns that accept allocator will cause illegal behavior
         pub fn initSentinelBuffer(
-            index_buffer: SentinelSlice,
-            id_buffer: SentinelSlice,
-            item_buffer: SentinelSlice,
+            comptime sentinel: T,
+            index_buffer: [:sentinel]T,
+            id_buffer: [:sentinel]T,
+            item_buffer: [:sentinel]T,
         ) @This() {
+            const len = item_buffer.len + 1;
+            assert(index_buffer.len == id_buffer.len);
+            assert(index_buffer.len == item_buffer.len);
             return @This(){
                 .indices = index_buffer[0..0],
                 .ids = id_buffer[0..0],
                 .items = item_buffer[0..0],
-                .capacity = 0,
+                .capacity = len,
             };
         }
 
@@ -73,6 +77,58 @@ pub fn Aligned(comptime T: type, comptime alignment: ?Alignment) type {
             allo.free(self.allocatedIdSlice());
             allo.free(self.allocatedItemSlice());
             self.* = undefined;
+        }
+
+        /// TriArrayList takes ownership of slice.
+        /// Create indices and ids using vectors and loops.
+        /// Deinit with `deinit` and `toOwnedSlice`.
+        pub fn fromOwnedSlice(allo: Allocator, slice: Slice) @This() {
+            const len = slice.len;
+            var indices = try allo.alignedAlloc(T, alignment, len);
+            var ids = try allo.alignedAlloc(T, alignment, len);
+            // use vectors to quickly create values
+            const VEC_LEN: comptime_int = 64;
+            const stair = std.simd.iota(usize, VEC_LEN);
+            var i: usize = 0;
+            while (i + 64 < len) : (i += 64) {
+                indices[i..][0..64].* = @as(@Vector(VEC_LEN, usize), @splat(i)) + stair;
+            }
+            if (i < len)
+                for (i..len) |j|
+                    indices[i] = j;
+            // copy indices to ids asymmetrically
+            @memcpy(ids, indices);
+            return @This(){
+                .indices = indices,
+                .ids = ids,
+                .items = items,
+                .capacity = capacity,
+            };
+        }
+
+        pub fn fromOwnedSentinelSlice(
+            comptime sentinel: T,
+            allo: Allocator,
+            slice: [:sentinel]T,
+        ) @This() {
+            const len = slice.len + 1;
+            var indices = try allo.alignedAlloc(usize, @alignOf(usize), len);
+            var ids = try allo.alignedAlloc(usize, @alignOf(usize), len);
+            // uses vectors to quickly create values
+            const VEC_LEN: comptime_int = 64;
+            const stair = std.simd.iota(usize, VEC_LEN);
+            var i: usize = 0;
+            while (i + 64 < len) : (i += 64) {
+                indices[i..][0..64].* = @as(@Vector(VEC_LEN, usize), @splat(i)) + stair;
+            }
+            if (i < len) for (i..len) |j| indices[i] = j;
+            @memcpy(ids, indices);
+            return @This(){
+                .indices = indices,
+                .ids = ids,
+                .items = slice,
+                .capacity = slice.len + 1,
+            };
         }
 
         /// Remove and return last item from items
@@ -89,60 +145,88 @@ pub fn Aligned(comptime T: type, comptime alignment: ?Alignment) type {
         /// Returns last item from items
         /// Asserts that items is not empty
         pub fn getLastItem(self: @This()) T {
-            if (@import("builtin").mode == .Debug) assert(self.items.len > 0);
             return self.items[self.items.len - 1];
         }
 
-        /// Returns last item from items or null if empty.
+        /// Returns last item or null if empty.
         pub fn getLastItemOrNull(self: @This()) ?T {
             return if (self.items.len > 0) self.items[self.items.len - 1] else null;
         }
 
-        pub fn getLastId(self: @This()) usize {
+        /// Returns id of the last item.
+        /// Asserts that items is not empty
+        pub fn getLastItemId(self: @This()) usize {
             return self.ids[self.items.len - 1];
         }
 
-        pub fn getLastIdOrNull(self: @This()) usize {
+        /// Returns id of last item or null if empty.
+        pub fn getLastItemIdOrNull(self: @This()) usize {
             return if (self.items.len > 0) self.ids[self.items.len - 1] else null;
         }
 
+        /// Returns last id.
+        /// Asserts that ids is not empty.
+        pub fn getLastId(self: @This()) usize {
+            return self.ids[self.ids.len - 1];
+        }
+
+        /// Returns last id or null.
+        pub fn getLastIdOrNull(self: @This()) usize {
+            return if (self.ids.len > 0) self.ids[self.ids.len - 1] else null;
+        }
+
         /// Get last index for last item
-        pub fn getLastIndex(self: @This()) usize {
+        pub fn getLastItemIndex(self: @This()) usize {
             return self.indices[self.items.len - 1];
         }
 
         /// Gets last index or null for last item
-        pub fn getLastIndexOrNull(self: @This()) usize {
+        pub fn getLastItemIndexOrNull(self: @This()) usize {
             return if (self.indices.len > 0) self.indices[self.items.len - 1] else null;
         }
 
+        /// Returns last index.
+        /// Asserts that index is not empty.
+        pub fn getLastIndex(self: @This()) usize {
+            return self.indices[self.indices.len - 1];
+        }
+
+        /// Returns last index or null if empty.
+        pub fn getLastIndexOrNull(self: @This()) usize {
+            return if (self.indices.len > 0) self.indices[self.indices.len - 1] else null;
+        }
+
+        /// Returns index slice
         pub fn allocatedIndexSlice(self: @This()) Slice {
             return self.indices.ptr[0..self.capacity];
         }
 
+        /// Returns id slice
         pub fn allocatedIdSlice(self: @This()) Slice {
             return self.ids.ptr[0..self.capacity];
         }
 
+        /// Returns item slice
         pub fn allocatedItemSlice(self: @This()) Slice {
             return self.items.ptr[0..self.capacity];
         }
 
-        pub fn unusedCapacityIndexSlice(self: @This()) []T {
+        /// Returns slice of unused indices up to capacity
+        pub fn unusedIndexSlice(self: @This()) []T {
             return self.allocatedIndexSlice()[self.indices.len..];
         }
 
-        pub fn unusedCapacityIdSlice(self: @This()) []T {
+        /// Returns slice of unused ids up to capacity
+        pub fn unusedIdSlice(self: @This()) []T {
             return self.allocatedIdSlice()[self.ids.len..];
         }
 
-        pub fn unusedCapacityItemsSlice(self: @This()) []T {
+        /// Returns slice of unused items up to capacity
+        pub fn unusedItemsSlice(self: @This()) []T {
             return self.allocatedItemSlice()[self.items.len..];
         }
 
         pub fn toOwnedSlice() []T {}
-
-        pub fn fromOwnedSlice() @This() {}
 
         pub fn append(self: *@This(), allo: Allocator) !void {
             if (self.items.len < self.capacity) {}
